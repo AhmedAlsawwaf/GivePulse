54fb638c0374a628797ac6c4a093ddfb2bbec01d
hlafayezabuGhali <hala2020fayez@gmail.com>
2025-10-04 12:25:04 +0300

    Add initial implementation of GivePulse platform with styling, templates, and routing

--- give_pulse_app/urls.py
+++ give_pulse_app/urls.py
@@ -2,5 +2,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index),
+    path("", views.index,name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("terms/", views.terms, name="terms"),
 ]
--- give_pulse_app/views.py
+++ give_pulse_app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render,HttpResponse
 
 def index(request):
     return render(request,"index.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def privacy(request):
+    return render(request, "privacy.html")
+
+def terms(request):
+    return render(request, "terms.html")
+
+def about(request):
+    return render(request, "about.html")
